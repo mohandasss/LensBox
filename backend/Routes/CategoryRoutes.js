53488717991acb7c718647f9117ffa002f144a21
mohandasss <gamermohan39@gmail.com>
2025-02-19 22:23:34 +0530

    Apis tested and updated.

--- backend/Routes/CategoryRoutes.js
+++ backend/Routes/CategoryRoutes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const { addCategory, updateCategory, deleteCategory, getCategoryById, getAllCategories } = require("../Controllers/CategoryController.js");
 
 router.post("/", addCategory);
-router.put("/:id", updateCategory);
 router.delete("/:id", deleteCategory);
 router.get("/:id", getCategoryById);
 router.get("/", getAllCategories);
